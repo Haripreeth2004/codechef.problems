da308ad376518ee90de41f77f2e081be6ac2fb0a
Haripreeth2004 <122097149+Haripreeth2004@users.noreply.github.com>
2023-07-21 03:29:16 -0700

    Create My codes.py

---
+++ My codes.py
@@ -0,0 +1,10 @@
+# cook your dish here
+testcase = int(input())
+for i in range (testcase):
+    tom = int(input("Speed of Tom"))
+    jerry =int(input("Speed of Jerry"))
+    if (tom>jerry):
+        print("YES")
+    else:
+        print("NO")
+    
