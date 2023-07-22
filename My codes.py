21921ced55b28b4cedf9f17355b4e46e7bc37f62
Haripreeth2004 <122097149+Haripreeth2004@users.noreply.github.com>
2023-07-22 07:39:10 +0530

    Delete My codes.py

--- My codes.py
+++
@@ -1,9 +0,0 @@
-testcase = int(input())
-for i in range (testcase):
-    jerrytom = input()
-    jerry, tom = map(int, jerrytom.split())
-    if (tom>jerry):
-        print("YES")
-    else:
-        print("NO")
-    
